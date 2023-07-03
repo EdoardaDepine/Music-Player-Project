bc31dbf666f7017e6461659756ab229ca2aa1f07
EdoardaDepine <edoarda.depine@gmail.com>
2023-07-03 15:24:35 -0300

    feat: criando esqueleto da página de músicas favoritas

---
+++ src/pages/favorites.js
@@ -0,0 +1,40 @@
+import { Component } from "react";
+import Header from "../components/header";
+import { getFavoriteSongs } from "../favoriteSongsAPI";
+import Loading from "../components/loading";
+import MusicCard from "../components/musicCard";
+//CSS
+
+class Favorites extends Component {
+  render() {
+    return (
+      <div className='pageAlbum'>
+        <Header />
+        {this.state.isLoading ? (
+          <Loading />
+        ) : (
+          <div>
+            {this.state.musics
+              .filter((music) => music.trackId)
+              .map((music) => (
+                <>
+                  <div className='cardMusicAndAlmbum'>
+                    <img alt='imagem do Álbum' src={music.artworkUrl100}></img>
+                    <MusicCard
+                      key={music.trackId}
+                      trackId={music.trackId}
+                      music={music}
+                      trackName={music.trackName}
+                      previewUrl={music.previewUrl}
+                    />
+                  </div>
+                </>
+              ))}
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default Favorites;
