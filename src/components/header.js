25173597642617bba36073eb60f5b4cc0819bf2d
EdoardaDepine <edoarda.depine@gmail.com>
2023-07-03 15:07:16 -0300

    feat: criando componente Header

---
+++ src/components/header.js
@@ -0,0 +1,41 @@
+import { Component } from "react";
+import { Link } from "react-router-dom";
+import { getUser } from "../userAPI";
+
+class Header extends Component {
+  render() {
+    return (
+      <header className='header'>
+        <div className='container'>
+          <p className='titleSite'>
+            <strong>Muisic</strong>tunes
+          </p>
+          <div className='userName'>
+            <p>
+              {this.state.user ? `user: ${this.state.user.name}` : <Loading />}
+            </p>
+          </div>
+        </div>
+        <div className='links'>
+          <div className='linkToSearch'>
+            <Link to='/search' className='search'>
+              Pesquisa
+            </Link>
+          </div>
+          <div className='linkToFavorites'>
+            <Link to='/favorites' className='favorites'>
+              Favoritas
+            </Link>
+          </div>
+          <div className='linkToProfile'>
+            <Link to='/profile' className='profile'>
+              Perfil
+            </Link>
+          </div>
+        </div>
+      </header>
+    );
+  }
+}
+
+export default Header;
